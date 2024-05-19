8273f8b45d52affe900f76c07b333beac5e02d6c
Akaki <a.titberidze@yahoo.com>
2024-05-19 06:20:04 +0400

    Readded feature

--- First-Collaboration-Refactoring/Scenes/Main/Weather/Models/CityDataModel.swift
+++ First-Collaboration-Refactoring/Scenes/Main/Weather/Models/CityDataModel.swift
@@ -1,8 +1,21 @@
 //
 //  CityDataModel.swift
-//  First-Collaboration-Refactoring
+//  Collaboration
 //
-//  Created by Barbare Tepnadze on 19.05.24.
+//  Created by Akaki Titberidze on 17.05.24.
 //
 
 import Foundation
+
+struct CityDataModel: Codable {
+    let results: [ResultWeatherModel]
+}
+
+struct ResultWeatherModel: Codable {
+    let geometry: Geometry
+}
+
+struct WeatherGeometry: Codable {
+    let lat: Double
+    let lng: Double
+}
