0a85958f0655de24a2af11cbdb4e4df95b4c7506
Solune <solneolune@protonmail.com>
2024-05-19 05:39:20 +0400

    add solarresource feature

--- First-Collaboration-Refactoring/Scenes/Main/SolarResource/SolarResourceModel.swift
+++ First-Collaboration-Refactoring/Scenes/Main/SolarResource/SolarResourceModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct SolarData: Codable {
+    let outputs: Outputs
+    
+    struct Outputs: Codable {
+        let avg_dni: AnnualMonthlyData
+        let avg_ghi: AnnualMonthlyData
+        let avg_lat_tilt: AnnualMonthlyData
+        
+        struct AnnualMonthlyData: Codable {
+            let annual: Double
+            let monthly: [String: Double]
+        }
+    }
+}
